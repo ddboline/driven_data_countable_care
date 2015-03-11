4bdbbe3a64c3127ee17ec237c7ff8d40f2bf3ad9
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-11 11:01:51 -0400

    ...

--- my_model.py
+++ my_model.py
@@ -189,7 +189,7 @@ def score_model(model, xtrain, ytrain):
     model.fit(xTrain, yTrain)
     #cvAccuracy = np.mean(cross_val_score(model, xtrain, ytrain, cv=2))
     ytest_pred = model.predict(xTest)
-    ytest_prob = model.predict_proba(xTest)[:,1]
+    ytest_prob = model.predict_proba(xTest)
     print ytest_pred
     print ytest_prob
     print yTest
