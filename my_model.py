d92c18abedb789866dffa2c3e668c1c8733df961
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-11 10:40:49 -0400

    ...

--- my_model.py
+++ my_model.py
@@ -213,8 +213,8 @@ if __name__ == '__main__':
     xtrain, ytrain, xtest, ytest = load_data()
 
 
-    model = RandomForestClassifier(n_estimators=2000, n_jobs=-1)
-    #model = LogisticRegression(class_weight='auto')
+    #model = RandomForestClassifier(n_estimators=2000, n_jobs=-1)
+    model = LogisticRegression(class_weight='auto')
     #model = SVC(kernel='linear', probability=True, verbose=False)
     #model = SGDRegressor()
     print score_model(model, xtrain, ytrain)
