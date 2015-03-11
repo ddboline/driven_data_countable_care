4c886d1c2d8692a751b8e23688a9ad9cf827f16a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-11 16:58:58 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -5,3 +5,4 @@
 tar zcvf output_${1}_`date +%Y%m%d%H%M%S`.tar.gz model_*.pkl.gz output.out output.err
 scp output_*.tar.gz ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/driven_data_countable_care/
 ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk done_driven_data_countable_care"
+sudo shutdown now
