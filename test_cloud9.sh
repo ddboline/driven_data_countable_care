0b8becafbfc480331e13381e0c7d2adde643028b
Daniel Boline <ddboline@gmail.com>
2015-03-11 14:42:37 -0400

    copy tar file to ddbolineathome after finished

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -3,3 +3,4 @@
 ./my_model.py $1 > output.out 2> output.err
 
 ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk done_driven_data_countable_care"
+scp model_*.pkg.gz ddboline@ddbolineathome.mooo.com:/home/ddboline/setup_files/build/driven_data_countable_care/
