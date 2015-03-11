d1d62984dcbda611a9b15c9574e5ac4e5d9cb5bc
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-11 11:08:04 -0400

    ...

--- my_model.py
+++ my_model.py
@@ -149,7 +149,7 @@ def load_data():
         #print c, train_df_values[c].dtype
 
     xtrain = train_df_values.values[:,1:]
-    ytrain = train_df_labels.values[:,1]
+    ytrain = train_df_labels.values[:,1:3]
     xtest = test_df_values.values[:,1:]
     ytest = test_df_labels
 
