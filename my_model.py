7bf34b69d10701b3c1456e457518e36883846ac1
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-11 09:48:45 -0400

    ...

--- my_model.py
+++ my_model.py
@@ -171,10 +171,10 @@ def score_model(model, xtrain, ytrain):
     xTrain, xTest, yTrain, yTest = \
       cross_validation.train_test_split(xtrain, ytrain, test_size=0.4,
                                         random_state=randint)
+    param_grid = {'penalty': ['l1', 'l2'], 'C': uniform(), }
     select = RFECV(estimator=model, scoring=scorer, step=0.1, verbose=1)
     clf = RandomizedSearchCV(estimator=select, 
-                                param_distributions={'penalty': ['l1', 'l2'],
-                                                     'C': uniform(), },
+                                param_distributions={'estimator_params': param_grid},
                                 scoring=scorer,
                                 n_jobs=-1, verbose=1)
     clf.fit(xTrain, yTrain)
